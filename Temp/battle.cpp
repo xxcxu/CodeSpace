09baebe317ebd43e3bc0cf8b8e51adf5b2d43251
Maraschinom <8950466@qq.com>
2022-09-21 19:01:12 +0800

    Update on SEP.21st

--- Temp/battle.cpp
+++ Temp/battle.cpp
@@ -1,30 +1,21 @@
-// 设 f[i][j][0/1] 为当前进行了i轮，我的左边有j人被打败，这轮我打/不打 我目前获胜的概率
 #include <bits/stdc++.h>
 static constexpr int N = 5e2 + 5;
-int n, K, a[N], l[N], r[N], p[N];
-double ans;
-auto erase(int x) -> void {
-    r[l[x]] = r[x];
-    l[r[x]] = l[x];
-}
+int n, K, a[N];
+double f[N][N], g[N][N], h[N][N];
 auto main() -> decltype(0) {
     freopen("battle.in", "r", stdin);
     freopen("battle.out", "w", stdout);
     scanf("%d%d", &n, &K);
-    for (int i = 1; i <= n; ++i) scanf("%d", a + i);
-    for (int i = 1; i <= n; ++i) p[i] = i;
-    do { 
-        if (p[n] != K) continue;
-        double res = 1.0;
-        for (int i = 1; i <= n; ++i) l[i] = i - 1, r[i] = i + 1;
-        for (int i = 1; i < n; ++i) {
-            double f1 = 0, f2 = 0;
-            if (l[p[i]] > 0) f1 = 1.0 * a[l[p[i]]] / (a[l[p[i]]] + a[p[i]]) / (n - i);
-            if (r[p[i]] < n + 1) f2 = 1.0 * a[r[p[i]]] / (a[r[p[i]]] + a[p[i]]) / (n - i);
-            res *= f1 + f2;
-            erase(p[i]);
+    for (int i = 1; i <= n; ++i)
+        scanf("%d", a + i), f[i][i] = g[i][i] = h[i][i] = 1.0;
+    for (int len = 2; len <= n; ++len)
+        for (int l = 1; l + len - 1 <= n; ++l) {
+            int r = l + len - 1;
+            for (int i = l; i < r; ++i) h[l][r] += f[l][i] * g[i + 1][r];
+            for (int i = l + 1; i <= r; ++i) f[l][r] += h[l][i] * f[i][r] * a[l] * 1.0 / (a[l] + a[i]);
+            for (int i = l; i <= r - 1; ++i) g[l][r] += g[l][i] * h[i][r] * a[r] * 1.0 / (a[r] + a[i]);
+            f[l][r] *= 1.0 / (r - l);
+            g[l][r] *= 1.0 / (r - l);
         }
-        ans += res;
-    } while(std::next_permutation(p + 1, p + n + 1));
-    return printf("%.17lf\n", ans), 0;
+    return printf("%.17lf\n", g[1][K] * f[K][n]), 0;
 }
