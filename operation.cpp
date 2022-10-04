9812bf850e20b1ff038470df609c291522153bab
Maraschinom <8950466@qq.com>
2022-10-04 19:43:53 +0800

    Update on Oct.4th

--- operation.cpp
+++ operation.cpp
@@ -1,25 +1,22 @@
-/*
- * @Author: Maraschino 
- * @Date: 2022-09-22 20:30:58 
- * @Last Modified by: Maraschino
- * @Last Modified time: 2022-09-22 20:36:02
- */
 #include <bits/stdc++.h>
 namespace Switch {
     #define function auto
     #define boolean bool
-    // #define FREOPEN // IO交互
-    // #define MULTIPLE // 多组数据
+    #define FREOPEN // IO交互
+    #define MULTIPLE // 多组数据
     // #define DEBUG // 调试信息
+    boolean isfreopen = false;
+    boolean ismultiple = false;
+    boolean isdebug = false;
 }
 namespace IO {
     const int precision = 5;
     namespace InputStream {
         template<typename T>
         inline function read() -> T {
-            T x = 0; boolean f = false; char ch = getchar();
+            T x = 0; boolean f = true; char ch = getchar();
             for (; !isdigit(ch); ch = getchar()) f ^= (ch == '-');
-            for (; isdigit(ch); ch = getchar()) x = (x << 1) + (x << 3) + (ch ^ 48);
+            for (; isdigit(ch); ch = getchar()) x = (x << 1) + (x << 3) - (ch ^ 48);
             return f ? ~x + 1 : x;
         }
         template<>
@@ -62,6 +59,8 @@ namespace IO {
         }
         function write(char x) -> void {putchar(x);}
         function write(std::string x) -> void {for (auto ch : x) putchar(ch);}
+        function write(char *x) -> void {puts(x);}
+        function write(const char *x) -> void {puts(x);}
         template<typename T>
         function write(T x) -> void {write(x);}
         template<typename T, typename ...Args>
@@ -75,100 +74,37 @@ using namespace IO::InputStream;
 using namespace IO::OutputStream;
 using ll = long long;
 namespace Codespace {
-    static constexpr int N = 2e2 + 5;
-    static constexpr ll inf = 0x3f3f3f3f3f3f3f;
-    int n, a[N]; ll l;
-    char s[N][N];
-    class Matrix {
-        private: int n, m;
-        public: std::vector<std::vector<ll>> a;
-        public: Matrix(int n = 0, int m = 0, int p = 0): n(n), m(m) {
-            a.resize(n, std::vector<ll>(m, -inf));
-            if (p) for (int i = 0; i < n; ++i) a[i][i] = 0;
-        }
-        public: std::vector<ll>& operator [] (int x) & {return a[x];}
-        public: const std::vector<ll>& operator[](int x) const& {return a[x];}
-        public: friend Matrix operator * (const Matrix &a, const Matrix &b) {
-            Matrix ans = Matrix(a.n, b.m);
-            for (int i = 0; i < a.n; ++i)
-                for (int j = 0; j < b.m; ++j)
-                    for (int k = 0; k < a.m; ++k)
-                        ans.a[i][j] = std::max(ans.a[i][j], a.a[i][k] + b.a[k][j]);
-            return ans;
-        }
-        public: function Unit() -> void {
-            assert(n == m);
-            for (int i = 0; i < n; ++i)
-                for (int j = 0; j < m; ++j) 
-                    a[i][j] = (i == j ? 0ll : -inf);
-            return void();
-        }
-        public: friend Matrix operator ^ (Matrix a, ll k) {
-            Matrix ans = Matrix(a.n, a.m, 1ll);
-            for (; k; k >>= 1, a = a * a) if (k & 1) ans = ans * a;
-            return ans;
-        }
-    };
-    class Acbot {
-        public: int cnt, ch[N][26], fail[N * 26];
-        public: ll con[N * 26];
-        public: function Insert(int o, int x) -> void {
-            int p = 0;
-            for (int i = 1; s[o][i]; ++i) {
-                int c = s[o][i] - 'a';
-                if (!ch[p][c]) ch[p][c] = ++ cnt;
-                p = ch[p][c];
-            }
-            con[p] += x;
-        }
-        public: function SpawnFail() -> void {
-            std::queue<int> q;
-            for (int i = 0; i < 26; ++i) if (ch[0][i]) q.emplace(ch[0][i]);
-            while (q.size()) {
-                int x = q.front(), y; q.pop();
-                for (int i = 0; i < 26; ++i)
-                    if (y = ch[x][i]; y) fail[y] = ch[fail[x]][i], q.push(y);
-                    else ch[x][i] = ch[fail[x]][i];
-                con[x] += con[fail[x]];
-            } return void();
-        }    
-    } Acr;
-    function Clear() -> void {}
-    function Solver(int test_case = 0) -> void {
-        read(n, l);
-        for (int i = 1; i <= n; ++i) read(a[i]);
-        for (int i = 1; i <= n; ++i) scanf("%s", s[i] + 1), Acr.Insert(i, a[i]);
-        Acr.SpawnFail();
-        Matrix A = Matrix(1, Acr.cnt + 1), Base = Matrix(Acr.cnt + 1, Acr.cnt + 1);
-        A[0][0] = 0;
-        for (int i = 0; i <= Acr.cnt; ++i)
-            for (int j = 0; j < 26; ++j)
-                Base[i][Acr.ch[i][j]] = std::max(Base[i][Acr.ch[i][j]], Acr.con[Acr.ch[i][j]]);
-        A = A * (Base ^ l);
-        ll ans = 0;
-        for (int i = 0; i <= Acr.cnt; ++i) ans = std::max(ans, A[0][i]);
-        write(ans, '\n');
-    }
+	static constexpr int N = 2e5 + 5, P = 998244353;
+	int n, k, ans, a[N], b[N], c[N];
+	function Solver(int test_case = 0) -> void {
+		read(n, k); ans = 1;
+		for (int i = 1; i <= n; ++i) read(a[i]), c[a[i]] = i;
+		for (int i = 1; i <= k; ++i) read(b[i]), a[i] = c[b[i]];
+		b[0] = 1, b[n + 1] = 1;
+		for (int i = 1; i <= n; ++i) b[i] = 0;
+		for (int i = k; i > 0; --i) {
+			b[a[i]] = 1;
+			ans = (ans * (2 - b[a[i] + 1] - b[a[i] - 1])) % P;
+		}
+		write(ans, '\n');
+	}
 }
-function main(int argv, char *argv[]) -> decltype(0) {
+function main(int argv, char *argc[]) -> decltype(0) {
     std::string path = __FILE__;
     std::string::size_type iPos = path.find_last_of('/') + 1;
     std::string filename = path.substr(iPos, path.length() - iPos);
     std::string realname = filename.substr(0, filename.rfind("."));
-    boolean isfreopen = false;
     #ifdef FREOPEN
         isfreopen = true;
     #endif
-    boolean ismultiple = false;
     #ifdef MULTIPLE
         ismultiple = true;
     #endif
-    boolean isdebug = false;
     #ifdef DEBUG
         isdebug = true;
     #endif
     int T, Case = 0;
-    if (isfreopen) freopenI(realname), freopenO(realname);
+    if (isfreopen) freopenI("operation"), freopenO("operation");
     if (ismultiple) read(T); else T = 1;
     while (T--) Codespace::Solver(++ Case);
     return 0;
